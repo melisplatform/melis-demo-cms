1e18291e1346b5002687cb7721eb2943a014a2ff
Kevin Suson <ksuson@melistechnology.com>
2018-03-06 14:05:38 +0800

    Mirrored changes from melis-installer

--- config/MelisDemoCms.config.php
+++ config/MelisDemoCms.config.php
@@ -1,42 +1,42 @@
 <?php
 
 return array(
-	'site' => array(
-		'MelisDemoCms' => array(
-			'conf' => array(
-				'id' => 'id_MelisDemoCms',
-			    'home_page' => 1
-			),
-			'datas' => array(
-			    // Site id
-			    'site_id' => 1,
-			    // Submenu limit
-			    'sub_menu_limit' => null,
-			    // News Page Id
-			    'news_menu_page_id' => 2,
-			    // News Details Page Id
-			    'news_details_page_id' => 3,
-			    // Testimonial parent id
-			    'testimonial_id' => 32,
-			    // Homepage header slider
-			    'homepage_header_slider' => 1,
-			    // Aboutus slider
-			    'aboutus_slider' => 2,
-			    // Search results page
-			    'search_result_page_id' => 36,
-			    /**
-			     * Required Modules for installation,
-			     * to trigger services that needed to install the MelisDemoCms
-			     * and to avoid deselect from selecting modules during installations.
-			     */
-			    'required_modules' => array(
-			        'MelisCmsNews',
-			        'MelisCmsSlider',
-			        'MelisCmsProspects',
-			    )
-			)	
-		)
-	),
+    'site' => array(
+        'MelisDemoCms' => array(
+            'conf' => array(
+                'id' => 'id_MelisDemoCms',
+                'home_page' => '[:homePageId]'
+            ),
+            'datas' => array(
+                // Site id
+                'site_id' => '[:siteId]',
+                // Submenu limit
+                'sub_menu_limit' => null,
+                // News Page Id
+                'news_menu_page_id' => '[:newsPageId]',
+                // News Details Page Id
+                'news_details_page_id' => '[:newsDetailsPageId]',
+                // Testimonial parent id
+                'testimonial_id' => '[:testimonialPageId]',
+                // Homepage header slider
+                'homepage_header_slider' => '[:homepageHeaderSlider]',
+                // Aboutus slider
+                'aboutus_slider' => '[:aboutusSlider]',
+                // Search results page
+                'search_result_page_id' => '[:searchResultsPageId]',
+                /**
+                 * Required Modules for installation,
+                 * to trigger services that needed to install the MelisDemoCms
+                 * and to avoid deselect from selecting modules during installations.
+                 */
+                'required_modules' => array(
+                    'MelisCmsNews',
+                    'MelisCmsSlider',
+                    'MelisCmsProspects',
+                )
+            )   
+        )
+    ),
     'plugins' => array(
         'melisfront' => array(
             'plugins' => array(
