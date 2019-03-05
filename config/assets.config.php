fd4a7b9fcaa74c8158c24c0353a7be3a4d7df28a
Cris <crismakabenta@gmail.com>
2019-03-05 19:39:39 +0800

    Added assets.config to configure all assets files

---
+++ config/assets.config.php
@@ -0,0 +1,25 @@
+<?php 
+
+return array(
+    'css' => array(
+        '/public/css/bootstrap.min.css',
+        '/public/css/core.css',
+        '/public/css/shortcode/shortcodes.css',
+        '/public/css/owl.carousel.css',
+        '/public/css/owl.theme.default.css',
+        '/public/css/owl.theme.green.min.css',
+        '/public/css/style.css',
+        '/public/css/responsive.css',
+        '/public/css/animate.css',
+        '/public/css/custom.css',
+        '/public/css/skin/skin-default.css',
+    ),
+    'js' => array(
+        '/public/js/vendor/jquery-2.2.4.min.js',
+        '/public/js/bootstrap.min.js',
+        '/public/js/owl.carousel.js',
+        '/public/js/jquery.countdown.min.js',
+        '/public/js/plugins.js',
+        '/public/js/main.js',
+    )
+);
