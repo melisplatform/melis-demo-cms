799767eeb0fde7b6920d79bf938fd03c8dcbff75
Junry Zabate <jzabajte@melistechnology.com>
2019-05-09 14:08:47 +0800

    fixes issue on can not detect viewport width method to use warn instead of throw on owl.carousel.js updated to console.warn see github: https://github.com/OwlCarousel2/ commit number #1705

--- public/js/owl.carousel.js
+++ public/js/owl.carousel.js
@@ -15,7 +15,7 @@
  * @todo Test Zepto
  * @todo stagePadding calculate wrong active classes
  */
-;(function($, window, document, undefined) {
+(function($, window, document, undefined) {
 
 	/**
 	 * Creates a carousel.
@@ -1274,7 +1274,7 @@
 		} else if (document.documentElement && document.documentElement.clientWidth) {
 			width = document.documentElement.clientWidth;
 		} else {
-			throw 'Can not detect viewport width.';
+			console.warn('Can not detect viewport width.');
 		}
 		return width;
 	};
