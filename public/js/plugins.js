697ce8f2d39b795a069c176596e6e9817a00d0d2
Junry Zabate <jzabajte@melistechnology.com>
2019-05-22 12:08:18 +0800

    files back to original copies for testing to fix console errors on microsoft edge which is an intermittent issue since previous tinymce version

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -72,7 +72,7 @@
     02. scrollup jquery 
 ---------------------------------------------------------------*/
 
-/*
+/*!
  * scrollup v2.4.1
  * Url: http://markgoodyear.com/labs/scrollup/
  * Copyright (c) Mark Goodyear — @markgdyr — http://markgoodyear.com
