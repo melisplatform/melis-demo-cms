ad966d4ac79d72e505072453fcf9bce702524674
fparan <fparan@melistechnology.com>
2020-01-22 15:45:02 +0800

    updated menu for the static and dynamic homepage

--- config/MelisDemoCms.config.php
+++ config/MelisDemoCms.config.php
@@ -5,8 +5,19 @@ return [
             '1' => [
                 'en_EN' => [
                     'homePageId' => 1,
+                    'static_home_page_id' => 34,
+                    'news_page_id' => 3,
+                    'news_details_page_id' => 4,
+                    'team_page_id' => 5,
+                    'services_page_id' => 6,
+                    'premium_plugins_page_id' => 7,
+                    'unique_elements_page_id' => 8,
+                    'live_page_builder_page_id' => 9,
+                    'process_page_id' => 10,
+                    'faq_page_id' => 14,
+                    'contact_page_id' => 21,
                     'testimonial_folder_id' => 23,
-                    'search_result_page_id' => 27
+                    'search_result_page_id' => 27,
                 ],
             ],
 
