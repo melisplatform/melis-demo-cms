a72dac9c0a5a4155075302fbb7ec90a7291b2359
fparan <fparan@melistechnology.com>
2020-01-27 17:17:48 +0800

    added editable fields for faq page

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -1,8 +1,5 @@
 <?php
 return [
-    'tr_demo_meliscms_faq_title_1' => 'How to purchase',
-    'tr_demo_meliscms_faq_title_2' => 'How to purchase',
-    'tr_demo_meliscms_faq_title_3' => 'How to purchase',
     'tr_demo_meliscms_search' => 'Search',
     'tr_demo_meliscms_contact_us_success' => '<strong>Success!</strong> Your message has been sent to us.',
     'tr_demo_meliscms_contact_us_error' => '<strong>Error!</strong> There was an error sending your message.',
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'tr_demo_meliscms_faq_title_1' => 'How to purchase',
     'tr_demo_meliscms_search' => 'Search',
     'tr_demo_meliscms_contact_us_success' => '<strong>Success!</strong> Your message has been sent to us.',
     'tr_demo_meliscms_contact_us_error' => '<strong>Error!</strong> There was an error sending your message.',
     'tr_demo_meliscms_news_details_latest_news' => 'Latest news',
-    'tr_demo_meliscms_no_result' => 'No result'
+    'tr_demo_meliscms_no_result' => 'No result',
+    'tr_demo_meliscms_no_data' => 'No data found'
 ];
