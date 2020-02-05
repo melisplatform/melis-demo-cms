7457120f1a3cdeb88a45ff85bb094f6d9c9f9545
fparan <fparan@melistechnology.com>
2020-02-05 12:53:13 +0800

    updated translations

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -5,5 +5,8 @@ return [
     'tr_demo_meliscms_contact_us_error' => '<strong>Error!</strong> There was an error sending your message.',
     'tr_demo_meliscms_news_details_latest_news' => 'Latest news',
     'tr_demo_meliscms_no_result' => 'No result',
-    'tr_demo_meliscms_no_data' => 'No data found'
+    'tr_demo_meliscms_no_data' => 'No data found',
+    'tr_demo_meliscms_search_result_no_dir' => '/vendor/melisplatform/melis-demo-cms/ directory doesn\'t exist',
+    'tr_demo_meliscms_search_result_not_indexed' => 'The search engine cannot proceed to a search as the website has not been indexed.<br>Please use the following URL to index your website',
+    'tr_demo_meliscms_search_result_lucene_index_not_writable' => 'melis-demo-cms/luceneIndex directory is not writable, please make the directory writable and try again',
 ];
