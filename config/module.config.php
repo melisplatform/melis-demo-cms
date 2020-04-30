e82df2a25a3698dd323d91385c641c32b356a78a
unknown <crismakabenta@gmail.com>
2020-04-30 15:38:17 +0800

    REmoved logger module

--- config/module.config.php
+++ config/module.config.php
@@ -113,7 +113,6 @@ return [
     'service_manager' => [
         'abstract_factories' => [
             \Laminas\Cache\Service\StorageCacheAbstractServiceFactory::class,
-            \Laminas\Log\LoggerAbstractServiceFactory::class,
         ],
         'aliases' => [
             'MelisPlatformTable' => \MelisDemoCms\Model\Tables\MelisPlatformTable::class,
